c495788b35646691b719193a30c0346f7427e19d
Balogun Abiodun <delighteddell@gmail.com>
2017-08-01 15:00:48 +0100

    Create subclass student

--- src/person.js
+++ src/person.js
@@ -17,5 +17,26 @@
     }
 }
 
-const person1 = new Person('Abbey','Male', 30,'Anthony','0703376565');
-console.log(person1.phoneNumber);
+//Class student inheriting from the super class
+class Student extends Person {
+    constructor(name, gender, age, address, phoneNumber, studentID, studentClass) {
+        super(name, gender, age, address, phoneNumber);
+        this.studentID = studentID;
+        this.studentClass = studentClass;
+    }
+    
+    displayStudentInfo() {
+      console.log(`Name: ${this.name}\nAge: ${this.age}\nPhone Number: ${this.phoneNumber}\nStudent ID: ${this.studentID}\nStudent Class: ${this.studentClass}`);  
+    }
+    
+    promoteStudent() {
+        this.studentClass += 1; 
+    }
+}
+
+console.log('\n\nStudent Class');
+const student1 = new Student('abbey', 'male', 30, 'lagos', '0909090', '13493', 3);
+student1.displayStudentInfo();
+student1.promoteStudent();
+console.log('\nAfter Promotion\n');
+student1.displayStudentInfo();
