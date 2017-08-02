5d0fdad0c56b4d9953b4b1eca0f9aeea820745e5
Balogun Abiodun <delighteddell@gmail.com>
2017-08-02 12:11:31 +0100

    modify class file and implement feedback

--- src/person.js
+++ src/person.js
@@ -1,51 +1,121 @@
+ /**
+  * @class Person
+  * @classdesc creates a new person instance
+  */
  class Person {
+    /**
+     * initializes the class props
+     * @param {String} name 
+     * @param {String} gender 
+     * @param {Number} age 
+     * @param {String} address 
+     * @param {String} phoneNumber 
+     */
     constructor(name, gender, age, address, phoneNumber) { 
-        // console.log('person class');
         this.name = name;
         this.gender = gender;
         this.age = age;
         this.address = address;
         this.phoneNumber = phoneNumber;
     }
-
+    
+    /**
+     * returns name 
+     * @return {String} name 
+     */
     getName() {
        return this.name; 
     }
 
+    /**
+     * Sets a new name
+     * @param {String} name
+     * @return {Void}
+     */
     setName(name) {
         this.name = name;
     }
 }
 
-//Class student inheriting from the super class
+ /**
+  * @class Student
+  * @classdesc creates a new Student instance and extends Person class
+  */
 class Student extends Person {
+
+    /**
+     * 
+     * @param {String} name 
+     * @param {String} gender 
+     * @param {Number} age 
+     * @param {String} address 
+     * @param {String} phoneNumber 
+     * @param {String} studentID 
+     * @param {Number} studentClass 
+     */
     constructor(name, gender, age, address, phoneNumber, studentID, studentClass) {
         super(name, gender, age, address, phoneNumber);
         this.studentID = studentID;
         this.studentClass = studentClass;
     }
     
+    /**
+     * Returns student information
+     * @return {String} Student Information
+     */
     displayStudentInfo() {
-      console.log(`Name: ${this.name}\nAge: ${this.age}\nPhone Number: ${this.phoneNumber}\nStudent ID: ${this.studentID}\nStudent Class: ${this.studentClass}`);  
+      return `Name: ${this.name}\nAge: ${this.age}
+        \nPhone Number: ${this.phoneNumber}
+        \nStudent ID: ${this.studentID}
+        \nStudent Class: ${this.studentClass}`;  
     }
     
+    /**
+     * Promotes student by increasing the class
+     * @return {Void}
+     */
     promoteStudent() {
         this.studentClass += 1; 
     }
 }
 
+/**
+ * @class Teacher
+ * @classdesc creates a new teacher instance and extends the Person class 
+ */
 class Teacher extends Person {
+    /**
+     * 
+     * @param {String} name 
+     * @param {String} gender 
+     * @param {Number} age 
+     * @param {String} address 
+     * @param {String} phoneNumber 
+     * @param {String} teacherID  
+     */
     constructor(name, gender, age, address, phoneNumber, teacherID){
       super(name, gender, age, address, phoneNumber);
       this.teacherID = teacherID;
       this.teacherSubjects = [];
     }
     
+    /**
+     * Returns Teacher's information
+     * @return {String} Teacher's Information
+     */
     displayTeacherInfo() {
-      console.log(`Name: ${this.name}\nAge: ${this.age}\nPhone Number: ${this.phoneNumber}\nTeacher ID: ${this.teacherID}\nSubjects: ${this.teacherSubjects}`); 
+      return `Name: ${this.name}
+        \nAge: ${this.age}
+        \nPhone Number: ${this.phoneNumber}
+        \nTeacher ID: ${this.teacherID}
+        \nSubjects: ${this.teacherSubjects}`; 
     }
     
-    //add subject
+    /**
+     * Adds subject to the class member of the teacher's class
+     * @param {Array | String} subjects
+     * @return {Void}
+     */
     addSubject(subjects) {
         if(Array.isArray(subjects)){
             this.teacherSubjects = this.teacherSubjects.concat(subjects);
@@ -54,11 +124,14 @@ class Teacher extends Person {
         }
     }
     
-    //Remove Subjects
+    /**
+     * Removes subject from the teacher's list of subjects
+     * @param {Number} subjectIndex
+     * @return {Array} teacher's subjects
+     */
     removeSubjects(subjectIndex) {
-        if(subjectIndex < 0  || subjectIndex >= this.teacherSubjects.length){
-          // console.log(subjectIndex);
-          console.log('\nInvalid subjects\n');
+        if (subjectIndex < 0  || subjectIndex >= this.teacherSubjects.length) {
+          return 'Invalid subjects';
         } else {
           this.teacherSubjects.splice(subjectIndex, 1);
         }
@@ -66,35 +139,6 @@ class Teacher extends Person {
     
 }
 
-
-
-console.log('\n\nStudent Class');
-const student1 = new Student('abbey', 'male', 30, 'lagos', '0909090', '13493', 3);
-student1.displayStudentInfo();
-student1.promoteStudent();
-console.log('\nAfter Promotion\n');
-student1.displayStudentInfo();
-
-
-const teacher1 = new Teacher('Wale', 'Male', 40, 'Andela', '0908883', 'TCH101');
-
-teacher1.addSubject('French');
-teacher1.addSubject(['Mathematics' , 'Biology']);
-teacher1.displayTeacherInfo();
-teacher1.removeSubjects(-1);
-
-
-console.log('\nAfer Failed Subject Removal\n');
-teacher1.displayTeacherInfo();
-teacher1.removeSubjects(1);
-
-console.log('\nAfer Successful Subject Removal\n');
-teacher1.displayTeacherInfo();
-
-
-
 module.exports = {
-    Person,
-    Student,
-    Teacher
-}
+    Person, Student, Teacher
+};
